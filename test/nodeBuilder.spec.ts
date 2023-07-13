bc17a8998618f5362025e86e62aabdfed25ef7f4
Michael Wu <68759331+micwu562@users.noreply.github.com>
2023-07-13 00:21:22 -0700

    Improved flow execution

--- test/nodeBuilder.spec.ts
+++ test/nodeBuilder.spec.ts
@@ -8,6 +8,19 @@ async function fetchApi() {
   };
 }
 
+async function fetchButDelayed(
+  ms
+): Promise<{ string: string; number: number }> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        string: "delayed!",
+        number: 400,
+      });
+    }, ms);
+  });
+}
+
 describe("Node Builder Test Suite", () => {
   test("Test dependency between nodes", async () => {
     const nodeBuilder = initNodeBuilder.create();
@@ -33,6 +46,49 @@ describe("Node Builder Test Suite", () => {
     expect(result.number2).toBe(2);
     expect(result.string2).toBe("stringsecond");
   });
+
+  test("Test concurrency", async () => {
+    const nodeBuilder = initNodeBuilder.create();
+
+    const firstNode = nodeBuilder.resolver(() => {
+      return fetchButDelayed(300);
+    });
+    const secondNode = nodeBuilder
+      .depend({
+        first: firstNode,
+      })
+      .resolver(async ({ deps }) => {
+        const { first } = deps;
+        const res = await fetchButDelayed(200);
+        return {
+          ...res,
+          firstData: first,
+        };
+      });
+    const thirdNode = nodeBuilder.resolver(() => {
+      return fetchButDelayed(400);
+    });
+
+    const fourthNode = nodeBuilder
+      .depend({
+        second: secondNode,
+        third: thirdNode,
+      })
+      .resolver(({ deps }) => {
+        const { second, third } = deps;
+        return {
+          string1: second.firstData.string,
+          string2: second.string,
+          string3: third.string,
+        };
+      });
+
+    const result = await fourthNode.run();
+
+    expect(result.string1).toBe("delayed!");
+    expect(result.string2).toBe("delayed!");
+    expect(result.string3).toBe("delayed!");
+  }, 600);
 
   test("Test error handling in nodes", async () => {
     const nodeBuilder = initNodeBuilder.create();
